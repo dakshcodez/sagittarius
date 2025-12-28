0effa63bc06db26cee6afe8aa4f5679f121f43df
dakshcodez <daksharoracodez@gmail.com>
2025-12-28 14:21:42 +0530

    added filemeta_test

--- internal/filemeta/filemeta_test.go
+++ internal/filemeta/filemeta_test.go
@@ -1,1 +1,34 @@
 package filemeta
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCreateFileMeta(t *testing.T) {
+	tmpFile, err := os.CreateTemp("", "chunk-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	data := []byte("this is a test file for chunking")
+	_, err = tmpFile.Write(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tmpFile.Close()
+
+	meta, err := CreateFileMeta(tmpFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if meta.NumChunks == 0 {
+		t.Fatal("expected chunks, got zero")
+	}
+
+	if meta.FileID == "" {
+		t.Fatal("file ID should not be empty")
+	}
+}
