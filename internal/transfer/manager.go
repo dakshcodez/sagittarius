8947240c9c41fe2e2ab69c38490d4a80648e1b11
dakshcodez <daksharoracodez@gmail.com>
2026-05-20 21:19:30 +0530

    feat: add META_REQUEST net msg and register meta func

--- internal/transfer/manager.go
+++ internal/transfer/manager.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"sync"
 
+	"github.com/dakshcodez/sagittarius/internal/filemeta"
 	"github.com/dakshcodez/sagittarius/internal/network"
 )
 
@@ -13,6 +14,7 @@ type TransferManager struct {
 
 	mu       sync.Mutex
 	sessions map[string]*DownloadSession // fileID -> session
+	metas    map[string]*filemeta.FileMeta
 }
 
 // NewTransferManager initializes a new transfer manager.
@@ -20,6 +22,7 @@ func NewTransferManager(selfID string) *TransferManager {
 	return &TransferManager{
 		selfID:   selfID,
 		sessions: make(map[string]*DownloadSession),
+		metas:    make(map[string]*filemeta.FileMeta),
 	}
 }
 
@@ -85,8 +88,81 @@ func (tm *TransferManager) HandleNetworkMessage(
 
 		return session.HandleChunkResponse(payload.ChunkIndex, payload.Data)
 
+	case "META_REQUEST":
+
+		var payload struct {
+			FileID string `json:"file_id"`
+		}
+
+		if err := json.Unmarshal(msg.Payload, &payload); err != nil {
+			return err
+		}
+
+		tm.mu.Lock()
+		meta, ok := tm.metas[payload.FileID]
+		tm.mu.Unlock()
+
+		if !ok {
+			return nil
+		}
+
+		metaPayload, err := json.Marshal(meta)
+		if err != nil {
+			return err
+		}
+
+		resp := network.Message{
+			Type:     "META_RESPONSE",
+			SenderID: tm.selfID,
+			Payload:  metaPayload,
+		}
+
+		return sender.Send(resp)	
+
+	case "META_RESPONSE":
+
+		var meta filemeta.FileMeta
+
+		if err := json.Unmarshal(msg.Payload, &meta); err != nil {
+			return err
+		}
+
+		tm.RegisterMeta(&meta)
+
+		return nil	
+
 	default:
 		// Ignore messages not handled by transfer layer
 		return nil
 	}
+}
+
+func (tm *TransferManager) RegisterMeta(meta *filemeta.FileMeta) {
+	tm.mu.Lock()
+	defer tm.mu.Unlock()
+
+	tm.metas[meta.FileID] = meta
+}
+
+func (tm *TransferManager) RequestMeta(
+	fileID string,
+	sender NetworkSender,
+) error {
+
+	payload, err := json.Marshal(struct {
+		FileID string `json:"file_id"`
+	}{
+		FileID: fileID,
+	})
+	if err != nil {
+		return err
+	}
+
+	msg := network.Message{
+		Type:     "META_REQUEST",
+		SenderID: tm.selfID,
+		Payload:  payload,
+	}
+
+	return sender.Send(msg)
 }
